9a91802ed0b0a533455e088a35c7887a16677119
Tatjana Svjatova <tsv.mailbox@gmail.com>
2020-06-08 20:50:55 +0300

    error

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,19 @@ import "./App.css";
 import PostMessages from "./components/PostMessages";
 import { Provider } from "react-redux";
 import { store } from "./actions/store";
+import { Container, AppBar, Typography } from "@material-ui/core";
 
 function App() {
   return (
     <Provider store={store}>
-      <PostMessages />
+      <Container maxWidth="lg">
+        <AppBar position="static" color="inherit">
+          <Typography variant="h2" align="center">
+            Post Box
+          </Typography>
+        </AppBar>
+        <PostMessages />
+      </Container>
     </Provider>
   );
 }
--- client/src/actions/postMessage.js
+++ client/src/actions/postMessage.js
@@ -1,12 +1,22 @@
+import api from "./api.js";
+
 export const ACTION_TYPES = {
   CREATE: "CREATE",
   UPDATE: "UPDATE",
   DELETE: "DELETE",
   FETCH_ALL: "FETCH_ALL",
 };
+
 export const fetchAll = () => (dispatch) => {
-  dispatch({
-    type: ACTION_TYPES_FETCH_ALL,
-    payload: [],
-  });
+  api
+    .postMessage()
+    .fetchAll()
+    .then((res) => {
+      console.log(res);
+      dispatch({
+        type: ACTION_TYPES.FETCH_ALL,
+        payload: res.data,
+      });
+    })
+    .catch((err) => console.log(err));
 };
